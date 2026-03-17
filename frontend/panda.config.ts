45fbf1d04e67a431088f95f8f083ccb87b5d108b
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2026-03-18 02:07:39 +0900

    feat: set up initial Next.js project with Panda CSS, ESLint, Prettier, and Husky configurations.

--- frontend/panda.config.ts
+++ frontend/panda.config.ts
@@ -1,13 +1,15 @@
-import {defineConfig} from "@pandacss/dev";
+import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}",
-        './src/components/**/*.{ts,tsx,js,jsx}',
-        './src/ui/**/*.{ts,tsx,js,jsx}'],
+  include: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{ts,tsx,js,jsx}',
+    './src/ui/**/*.{ts,tsx,js,jsx}',
+  ],
 
   // Files to exclude
   exclude: [],
@@ -18,5 +20,5 @@ export default defineConfig({
   },
 
   // The output directory for your css system
-  outdir: "styled-system",
+  outdir: 'styled-system',
 });
